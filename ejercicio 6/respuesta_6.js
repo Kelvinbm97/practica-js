cf280a491c9661c534924181a04c57585bf2aa0e
Jhornan <jhornankevinmallqui@gmail.com>
2022-02-02 04:24:29 -0500

    respuesta de la pregunta 6

---
+++ ejercicio 6/respuesta_6.js
@@ -0,0 +1,17 @@
+const persona = {
+  nombre: "Jhornan",
+  celular: 77584562,
+  estudiante: true
+}
+
+const curso = {
+  asignatura : "Fisica",
+  creditos: 3,
+  aprobados:true
+}
+
+const pais ={
+  nombre : "España",
+  idioma : 2,
+  primermundista : true
+}
