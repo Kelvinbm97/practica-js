ab71387bf31c368c366a9bac403fcf1d1a199d5f
Jhornan <jhornankevinmallqui@gmail.com>
2022-02-04 12:13:06 -0500

    respuesta de la pregunta 6

--- ejercicio 6/respuesta_6.js
+++ ejercicio 6/respuesta_6.js
@@ -1,17 +1,49 @@
 const persona = {
   nombre: "Jhornan",
+  edad: 22,
   celular: 77584562,
   estudiante: true,
+  cursosFavorito : ["Fisica", "Matematica","Algebra"],
+  amigos : {
+    mejorAmigo : {
+      nombre: "Pepe",
+      edad:20
+    },
+    mejorAmiga :{
+      nombre: "Josefina",
+      edad : 22
+    }
+  },
+  hobiies : ["Natacion", "Correr"]
+
 }
 
 const curso = {
   asignatura : "Fisica",
   creditos: 3,
   aprobados:true,
+  horario : ["Tarde", "Mañana"],
+  docente : {
+      nombre: "Luis",
+      edad: 45
+  }
 }
 
 const pais ={
   nombre : "España",
-  idioma : 2,
+  idioma : ["Español", "Catalan"],
   primermundista : true,
+  provincias : 50,
+  departamentos : {
+    Valencia : {
+      alcalde : "Joan Ribó"
+    },
+    Barcelona : {
+      alcalde: "Ada Colau"
+    },
+    Madrid: {
+      alclade: "Jose Luis Martinez"
+    }
+  }
+
 }
